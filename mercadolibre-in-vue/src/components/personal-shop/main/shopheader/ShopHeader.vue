ad7d95c46ff44d4de8e05345e91c64ab28209e3d
Juan Gallo <juanfgallo@experimentality.co>
2020-09-17 00:02:51 -0500

    avances

--- mercadolibre-in-vue/src/components/personal-shop/main/shopheader/ShopHeader.vue
+++ mercadolibre-in-vue/src/components/personal-shop/main/shopheader/ShopHeader.vue
@@ -1,36 +1,20 @@
 <template>
   <v-card class="overflow-hidden">
-    <v-app-bar
-      absolute
-      color="primary"
-      dark
-    >
-      <v-app-bar-nav-icon color="#3B3815"></v-app-bar-nav-icon>
-
+    <v-app-bar>
+      <div>
+        <v-img class="ml-logo" :src="require('@/assets/ml.png')" />
+      </div>
       <v-toolbar-title class="titulo">Gallito Shop</v-toolbar-title>
-
       <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon color="#3B3815">mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon color="#3B3815">mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon color="#3B3815">mdi-dots-vertical</v-icon>
-      </v-btn>
+      <div>
+        <v-img class="aniversario" :src="require('@/assets/aniversario.png')" />
+      </div>
     </v-app-bar>
     <v-sheet
       id="scrolling-techniques-8"
       class="overflow-y-auto"
       max-height="600"
     >
-      <v-container style="height: 1500px;">
-
-      </v-container>
     </v-sheet>
   </v-card>
 </template>
@@ -39,8 +23,8 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "ShopHeader"
-})
+});
 </script>
 <style scoped>
-  @import "./ShopHeader.css";
+@import "./ShopHeader.css";
 </style>
